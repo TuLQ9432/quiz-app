67d8a20011a30084d6be690d52ff6599752547b1
Tu Le Quang <tulq9432@gmail.com>
2022-05-10 00:00:13 +0700

    Import Roboto webfont (400-italic)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/400-italic.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import CssBaseline from "@mui/material/CssBaseline";
